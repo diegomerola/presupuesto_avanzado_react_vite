3891d9f0cf4a214700a65420d73febdb67892085
diegomerola <diego_merola@hotmail.com>
2022-02-02 12:28:43 -0300

    Cambiando la apariencia segun el porcentaje gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -55,9 +55,9 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
         value={porcentaje}
         text={`${porcentaje} % Gastado`}
         styles={buildStyles({
-          pathColor: "#3B82F6",
+          pathColor: porcentaje > 100 ? "#DC2626" : "#3B82F6",
           trailColor: "#F5F5F5",
-          textColor: "#3B82F6",
+          textColor: porcentaje > 100 ? "#DC2626" : "#3B82F6",
         })}
       />
       <div className="contenido-presupuesto">
@@ -65,7 +65,7 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
           <span>Presupuesto:</span>
           {formatearCantidad(presupuesto)}
         </p>
-        <p>
+        <p className={`${disponible < 0 ? "negativo" : ""}`}>
           <span>Disponible:</span>
           {formatearCantidad(disponible)}
         </p>
